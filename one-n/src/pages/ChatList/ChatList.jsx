4689c10a2301312d972c8bb9f204198ea0fbbe74
ypjun100 <ypjun100@gmail.com>
2025-01-28 12:38:17 +0900

    ref: 이전 채팅방 리스트 코드 제거

--- one-n/src/pages/ChatList/ChatList.jsx
+++ one-n/src/pages/ChatList/ChatList.jsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 
 import { ChatRoomsProvider } from "./contexts/ChatRoomsContext";
 
@@ -11,62 +9,6 @@ import List from "./sub-components/List/List";
 import styles from "./ChatList.module.scss";
 
 function ChatList() {
-    // const baseUrl = "https://n1.junyeong.dev/api";
-    // const navigate = useNavigate();
-
-    // const [chat, setChat] = useState([]);
-    // const [unreadCounts, setUnreadCounts] = useState({});
-
-    // const [signinData, setSigninData] = useState(null);
-
-    // useEffect(() => {
-    //     const storedSigninData = sessionStorage.getItem("signinData");
-    //     if (storedSigninData) {
-    //         setSigninData(JSON.parse(storedSigninData));
-    //         console.log(signinData);
-    //     }
-    // }, []);
-
-    // const handleChatClick = (chatId) => {
-    //     navigate(`/chatroom/${chatId}`);
-    // };
-
-    // useEffect(() => {
-    //     // 참가 채팅방
-    //     const apiUrl = `${baseUrl}/chats/list?session_id=test_session_id`;
-    //     // 미확인 채팅
-    //     const unreadApiUrl = `${baseUrl}/chats/unread-messages?session_id=test_session_id`;
-
-    //     // 채팅 목록
-    //     axios
-    //         .get(apiUrl)
-    //         .then((response) => {
-    //             setChat(response.data);
-    //             console.log(response.data);
-    //         })
-    //         .catch((error) => {
-    //             console.error("API 요청 에러:", error);
-    //         });
-
-    //     // 안 읽은 채팅 개수
-    //     axios
-    //         .get(unreadApiUrl)
-    //         .then((response) => {
-    //             // chat_id를 기준으로 그룹화, 채팅방마다 안 읽은 메시지 갯수를 카운트
-    //             const unreadCountsData = response.data.reduce(
-    //                 (counts, chat) => {
-    //                     counts[chat.chat_id] = (counts[chat.chat_id] || 0) + 1;
-    //                     return counts;
-    //                 },
-    //                 {}
-    //             );
-    //             setUnreadCounts(unreadCountsData);
-    //         })
-    //         .catch((error) => {
-    //             console.error("API 요청 에러:", error);
-    //         });
-    // }, []);
-
     return (
         <ChatRoomsProvider>
             <div className={styles.ChatList}>
