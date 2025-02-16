10cd8af09e2ff3aafcdb8bb317b919795dcab044
ypjun100 <ypjun100@gmail.com>
2025-02-16 12:35:32 +0900

    feat: 채팅방 메시지 컴포넌트 구현

--- one-n/src/pages/ChatRoom/sub-components/MessageList/MessageList.jsx
+++ one-n/src/pages/ChatRoom/sub-components/MessageList/MessageList.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+import styles from "./MessageList.module.scss";
+import UserChat from "./sub-components/UserChat/UserChat";
+
 function MessageList() {
-    return <div>Message List</div>;
+    return (
+        <div className={styles.MessageList}>
+            <UserChat self messageTime="19:24">
+                넵 5kg에 20,900원짜리 구매 예정이고
+            </UserChat>
+            <UserChat self messageTime="19:24">
+                국민 0000-00000000으로 10,450원 보내주시면 됩니다!
+            </UserChat>
+            <UserChat userName="군고구마애호가" messageTime="19:24">
+                위치는 어디서 만나면 될까요?
+            </UserChat>
+            <UserChat self messageTime="19:24">
+                거래는 강남역 10번 출구 입니다!
+            </UserChat>
+            <UserChat userName="자연사랑나라사랑" messageTime="19:24">
+                넵!
+            </UserChat>
+            <UserChat userName="자연사랑나라사랑" messageTime="19:24">
+                zzzzxcvzxcvzxcvzxcvzxcvzxcvzxcvzxcvzxc
+            </UserChat>
+        </div>
+    );
 }
 
 export default MessageList;
---
+++ one-n/src/pages/ChatRoom/sub-components/MessageList/sub-components/UserChat/UserChat.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import cn from "classnames";
+
+import MessageCard from "./sub-components/MessageCard/MessageCard";
+
+import styles from "./UserChat.module.scss";
+
+function UserChat({
+    self = false,
+    profileImageUrl = "",
+    userName = "",
+    messageTime = "",
+    children,
+}) {
+    if (self) {
+        return (
+            <div className={cn(styles.UserChat, styles.self)}>
+                <div className={styles.nameAndMessage}>
+                    <div className={styles.messageAndTime}>
+                        <p className={styles.messageTime}>{messageTime}</p>
+                        <MessageCard self={self} message={children} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className={styles.UserChat}>
+            <img className={styles.profileImage} src={profileImageUrl} />
+            <div className={styles.nameAndMessage}>
+                <p className={styles.name}>{userName}</p>
+                <div className={styles.messageAndTime}>
+                    <MessageCard self={self} message={children} />
+                    <p className={styles.messageTime}>{messageTime}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default UserChat;
