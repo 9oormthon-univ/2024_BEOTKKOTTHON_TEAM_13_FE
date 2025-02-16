55749f5478ad8deb9165bacc4e353269a857e3ea
ypjun100 <ypjun100@gmail.com>
2025-02-16 10:15:19 +0900

    feat: 조건 하단바 로직 변경

--- one-n/src/App.js
+++ one-n/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import MoreProductPage from './pages/MoreProductPage/MoreProductPage';
-import CheckLocationPage from './pages/CheckLocationPage/CheckLocationPage';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useLocation,
+} from "react-router-dom";
+import MoreProductPage from "./pages/MoreProductPage/MoreProductPage";
+import CheckLocationPage from "./pages/CheckLocationPage/CheckLocationPage";
 import { NavBar } from "./components/NavBar/NavBar";
 import Map from "./pages/Map/Map";
 import ProductDetail from "./pages/ProductDetail/ProductDetail";
@@ -23,43 +28,75 @@ import Signup from "./pages/Signup/Signup";
 import EditProfile from "./pages/EditProfile/EditProfile"; // 추가된 import
 
 function App() {
-  return (
-    <Router>
-      <MyContextProvider>
-        <div className="App">
-          <Routes>
-            <Route path="/product-detail/:productId" element={<ProductDetail />} />
-            <Route path="/reproduct-detail/:productId" element={<ReProductDetail />} />
-            <Route path="/map" element={<Map />} />
-            <Route path="/search-address" element={<SearchAddress />} />
-            <Route path="/select-location" element={<SelectLocation />} />
-            <Route path="/product-post" element={<ProductPost />} />
-            <Route path="/scrap" element={<Scrap />} />
-            <Route path="/" element={<Signin />} />
-            <Route path="/home" element={<MainPage />} />
-            <Route path="/chat" element={<ChatList />} />
-            <Route path="/explore" element={<Explore />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/more-product" element={<MoreProductPage />} />
-            <Route path="/check-location" element={<CheckLocationPage />} />
-            <Route path="/edit-profile" element={<EditProfile />} />
-            <Route path="/chatroom/:chatId" element={<ChatRoom />} />
-            <Route path="/recipe/:recipeId" element={<RecipeDetail />} />
-            <Route path="/recipeRegister" element={<RecipeReg />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-          <ConditionalNavBar />
-        </div>
-      </MyContextProvider>
-    </Router>
-  );
+    return (
+        <Router>
+            <MyContextProvider>
+                <div className="App">
+                    <Routes>
+                        <Route
+                            path="/product-detail/:productId"
+                            element={<ProductDetail />}
+                        />
+                        <Route
+                            path="/reproduct-detail/:productId"
+                            element={<ReProductDetail />}
+                        />
+                        <Route path="/map" element={<Map />} />
+                        <Route
+                            path="/search-address"
+                            element={<SearchAddress />}
+                        />
+                        <Route
+                            path="/select-location"
+                            element={<SelectLocation />}
+                        />
+                        <Route path="/product-post" element={<ProductPost />} />
+                        <Route path="/scrap" element={<Scrap />} />
+                        <Route path="/" element={<Signin />} />
+                        <Route path="/home" element={<MainPage />} />
+                        <Route path="/chat" element={<ChatList />} />
+                        <Route path="/explore" element={<Explore />} />
+                        <Route path="/search" element={<SearchPage />} />
+                        <Route
+                            path="/more-product"
+                            element={<MoreProductPage />}
+                        />
+                        <Route
+                            path="/check-location"
+                            element={<CheckLocationPage />}
+                        />
+                        <Route path="/edit-profile" element={<EditProfile />} />
+                        <Route
+                            path="/chatroom/:chatId"
+                            element={<ChatRoom />}
+                        />
+                        <Route
+                            path="/recipe/:recipeId"
+                            element={<RecipeDetail />}
+                        />
+                        <Route path="/recipeRegister" element={<RecipeReg />} />
+                        <Route path="/signup" element={<Signup />} />
+                    </Routes>
+                    <ConditionalNavBar />
+                </div>
+            </MyContextProvider>
+        </Router>
+    );
 }
 
 function ConditionalNavBar() {
-  const location = useLocation();
-  const noNavBarRoutes = ["/", "/signup"]; // Navbar를 표시하지 않을 경로
+    const location = useLocation();
+    const hideNavBarRoutes = [/^\/$/, /^\/signup/, /^\/chatroom/]; // Navbar를 표시하지 않을 경로
 
-  return !noNavBarRoutes.includes(location.pathname) ? <NavBar /> : null;
+    const isNavBarHide = hideNavBarRoutes.some(
+        (route) => !!location.pathname.match(route)
+    );
+
+    if (isNavBarHide) {
+        return null;
+    }
+
+    return <NavBar />;
 }
 
 export default App;
