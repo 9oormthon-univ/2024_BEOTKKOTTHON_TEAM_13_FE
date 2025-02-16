234b80ab153853f3097e8d316fabedb71f9655db
ypjun100 <ypjun100@gmail.com>
2025-02-16 09:50:26 +0900

    feat: 채팅방 서브 헤더의 상품 목록 UI 구현

--- one-n/src/pages/ChatRoom/sub-components/Header/sub-components/SubHeader/sub-components/PopoverButton/PopoverButton.jsx
+++ one-n/src/pages/ChatRoom/sub-components/Header/sub-components/SubHeader/sub-components/PopoverButton/PopoverButton.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import cn from "classnames";
 
 import { ReactComponent as BasketIcon } from "../../../../../../../../assets/icons/basket.svg";
 
 import styles from "./PopoverButton.module.scss";
 
 function PopoverButton() {
+    const [isPopoverActive, setPopoverActive] = useState(false);
+
+    const togglePopoverActive = () => setPopoverActive((prev) => !prev);
+
     return (
-        <div className={styles.PopoverButton}>
+        <div
+            className={cn(
+                styles.PopoverButton,
+                isPopoverActive && styles.active
+            )}
+            onClick={togglePopoverActive}
+        >
             <div className={styles.container}>
                 <BasketIcon width={16} height={16} />
                 <p>+4</p>
@@ -14,7 +25,7 @@ function PopoverButton() {
             <div className={styles.popover}>
                 <div className={styles["popover-elements"]}>
                     <p>당근</p>
-                    <p>닭고기ㅋㅋㅋㅋㅋ</p>
+                    <p>닭고기</p>
                     <p>고구마</p>
                     <p>양파</p>
                 </div>
