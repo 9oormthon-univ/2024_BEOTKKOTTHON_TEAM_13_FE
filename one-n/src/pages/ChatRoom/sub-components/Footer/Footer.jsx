f0b7fe27cfcd48111345ecbc633d7bdc66b52ac5
ypjun100 <ypjun100@gmail.com>
2025-02-16 11:39:34 +0900

    feat: 채팅방 메시지 입력 푸터 구현

--- one-n/src/pages/ChatRoom/sub-components/Footer/Footer.jsx
+++ one-n/src/pages/ChatRoom/sub-components/Footer/Footer.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
+import ImageButton from "../../../../components/ImageButton/ImageButton";
+import MessageInput from "./sub-components/MessageInput/MessageInput";
+
+import { ReactComponent as PhotoIcon } from "../../../../assets/icons/photo.svg";
+import { ReactComponent as SendIcon } from "../../../../assets/icons/send.svg";
+
+import styles from "./Footer.module.scss";
+
 function Footer() {
-    return <div>Footer</div>;
+    const onUploadPhoto = () => {
+        console.log("Upload photo");
+    };
+
+    const onSendMessage = () => {
+        console.log("Send message");
+    };
+
+    return (
+        <div className={styles.Footer}>
+            <ImageButton
+                size="md"
+                color="grey"
+                icon={<PhotoIcon />}
+                onClick={onUploadPhoto}
+            />
+            <MessageInput />
+            <ImageButton
+                size="md"
+                color="yellow"
+                icon={<SendIcon />}
+                onClick={onSendMessage}
+            />
+        </div>
+    );
 }
 
 export default Footer;
