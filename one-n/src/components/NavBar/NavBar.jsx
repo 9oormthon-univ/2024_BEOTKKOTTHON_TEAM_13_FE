c780948e675e2deb4a5159fb1dbc53de567218ed
parknari02 <pnr021118@naver.com>
2025-03-24 22:40:17 +0900

    style: footer 아이콘 및 스타일 수정

--- one-n/src/components/NavBar/NavBar.jsx
+++ one-n/src/components/NavBar/NavBar.jsx
@@ -1,68 +1,77 @@
 import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import "./NavBar.css";
-import home from "../../assets/icons/nav-home.png";
-import explore from "../../assets/icons/nav-explore.png";
-import chat from "../../assets/icons/nav-chat.png";
-import map from "../../assets/icons/nav-map.png";
-import inhome from "../../assets/icons/nav-in-home.png";
-import inexplore from "../../assets/icons/nav-in-explore.png";
-import inchat from "../../assets/icons/nav-in-chat.png";
-import inmap from "../../assets/icons/nav-in-map.png";
+import home from "../../assets/footer-icons/basic/home.svg";
+import explore from "../../assets/footer-icons/basic/explore.svg";
+import chat from "../../assets/footer-icons/basic/chat.svg";
+import map from "../../assets/footer-icons/basic/map.svg";
+import inhome from "../../assets/footer-icons/active/home.svg";
+import inexplore from "../../assets/footer-icons/active/explore.svg";
+import inchat from "../../assets/footer-icons/active/chat.svg";
+import inmap from "../../assets/footer-icons/active/map.svg";
 
 export const NavBar = () => {
   const location = useLocation();
 
   return (
     <div className="navbar">
-      <NavLink
-        exact
-        to="/home"
-        className="nav-home"
-        isActive={() => location.pathname === "/"}
-      >
-        {location.pathname === "/home" ? (
-          <img src={inhome} alt="home" />
-        ) : (
-          <img src={home} alt="home" />
-        )}
-      </NavLink>
-
-      <NavLink
-        to="/explore"
-        className="nav-explore"
-        isActive={() => location.pathname === "/explore"}
-      >
-        {location.pathname === "/explore" ? (
-          <img src={inexplore} alt="explore" />
-        ) : (
-          <img src={explore} alt="explore" />
-        )}
-      </NavLink>
-
-      <NavLink
-        to="/map"
-        className="nav-map"
-        isActive={() => location.pathname === "/map"}
-      >
-        {location.pathname === "/map" ? (
-          <img src={inmap} alt="map" />
-        ) : (
-          <img src={map} alt="map" />
-        )}
-      </NavLink>
-
-      <NavLink
-        to="/chat"
-        className="nav-chat"
-        isActive={() => location.pathname === "/chat"}
-      >
-        {location.pathname === "/chat" ? (
-          <img src={inchat} alt="chat" />
-        ) : (
-          <img src={chat} alt="chat" />
-        )}
-      </NavLink>
+      <div className="icon-cont">
+        <NavLink
+          exact
+          to="/home"
+          className="nav-home"
+          isActive={() => location.pathname === "/"}
+        >
+          {location.pathname === "/home" ? (
+            <img src={inhome} alt="home" />
+          ) : (
+            <img src={home} alt="home" />
+          )}
+        </NavLink>
+        <span className="icon-title">홈</span>
+      </div>
+      <div className="icon-cont">
+        <NavLink
+          to="/explore"
+          className="nav-explore"
+          isActive={() => location.pathname === "/explore"}
+        >
+          {location.pathname === "/explore" ? (
+            <img src={inexplore} alt="explore" />
+          ) : (
+            <img src={explore} alt="explore" />
+          )}
+        </NavLink>
+        <span className="icon-title">레시피</span>
+      </div>
+      <div className="icon-cont">
+        <NavLink
+          to="/map"
+          className="nav-map"
+          isActive={() => location.pathname === "/map"}
+        >
+          {location.pathname === "/map" ? (
+            <img src={inmap} alt="map" />
+          ) : (
+            <img src={map} alt="map" />
+          )}
+        </NavLink>
+        <span className="icon-title">지도</span>
+      </div>
+      <div className="icon-cont">
+        <NavLink
+          to="/chat"
+          className="nav-chat"
+          isActive={() => location.pathname === "/chat"}
+        >
+          {location.pathname === "/chat" ? (
+            <img src={inchat} alt="chat" />
+          ) : (
+            <img src={chat} alt="chat" />
+          )}
+        </NavLink>
+        <span className="icon-title">채팅</span>
+      </div>
     </div>
 
     // <div className="navbar">
