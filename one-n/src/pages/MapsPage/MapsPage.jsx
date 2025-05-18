7394f33b0f6d8902acab58bd385baa5275c00284
ypjun100 <ypjun100@gmail.com>
2025-05-18 12:10:16 +0900

    feat: 지도 현재 위치 초기화 버튼 구현

--- one-n/src/pages/MapsPage/MapsPage.jsx
+++ one-n/src/pages/MapsPage/MapsPage.jsx
@@ -3,8 +3,13 @@ import { Map, useKakaoLoader } from "react-kakao-maps-sdk";
 
 import SearchBar from "./sub-components/SearchBar/SearchBar";
 import BottomMenu from "./sub-components/BottomMenu/BottomMenu";
+import PositionResetButton from "./sub-components/PositionResetButton/PositionResetButton";
 
-import useUserLocation from "./hooks/useUserLocation";
+import {
+    MapsProvider,
+    useMapsValue,
+    useMapsAction,
+} from "./contexts/MapsContext";
 
 import styles from "./MapsPage.module.scss";
 
@@ -13,23 +18,39 @@ function MapsPage() {
         appkey: process.env.REACT_APP_KAKAO_JS_KEY,
         libraries: ["services", "clusterer"],
     });
-    const userLocation = useUserLocation();
+    const { mapRef, centerPosition } = useMapsValue();
+    const { setCenterPosition } = useMapsAction();
 
     return (
         <div className={styles.MapsPage}>
             <SearchBar />
             <Map
+                ref={mapRef}
                 className={styles.map}
-                center={{
-                    lat: userLocation.latitude,
-                    lng: userLocation.longitude,
-                }}
+                center={centerPosition}
                 isPanto={true}
                 level={5}
+                onDragEnd={(map) => {
+                    // NOTE: 드래그 종료 시점에 지도 중심 위치를 업데이트
+                    const center = map.getCenter();
+                    setCenterPosition({
+                        lat: center.getLat(),
+                        lng: center.getLng(),
+                    });
+                }}
             />
             <BottomMenu />
+            <PositionResetButton />
         </div>
     );
 }
 
-export default MapsPage;
+const withProviders = (WrappedComponent) => (props) => {
+    return (
+        <MapsProvider>
+            <WrappedComponent {...props} />
+        </MapsProvider>
+    );
+};
+
+export default withProviders(MapsPage);
